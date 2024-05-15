49a791cc6b6ad2277113e2bd01af2e164b9130c5
Anas Abdrahman <mynasmah@gmail.com>
2024-05-16 02:20:51 +0800

    fix function not exists

--- swipego-wc.php
+++ swipego-wc.php
@@ -15,11 +15,13 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 if ( class_exists( 'Swipego_WC' ) ) return;
 
+if ( ! function_exists('swipego_get_integration') ) return;
+
 define( 'SWIPEGO_WC_FILE', __FILE__ );
 define( 'SWIPEGO_WC_URL', plugin_dir_url( SWIPEGO_WC_FILE ) );
 define( 'SWIPEGO_WC_PATH', plugin_dir_path( SWIPEGO_WC_FILE ) );
 define( 'SWIPEGO_WC_BASENAME', plugin_basename( SWIPEGO_WC_FILE ) );
-define( 'SWIPEGO_WC_VERSION', '1.0.0' );
+define( 'SWIPEGO_WC_VERSION', '1.0.2' );
 
 // Plugin core class
 require( SWIPEGO_WC_PATH . 'includes/class-swipego-wc.php' );
