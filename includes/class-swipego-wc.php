7f9c537fde92d7044897599fea032f60ce2ef654
Anas Abdrahman <mynasmah@gmail.com>
2024-05-16 00:00:21 +0800

    update fix - expired token

--- includes/class-swipego-wc.php
+++ includes/class-swipego-wc.php
@@ -8,35 +8,36 @@ class Swipego_WC {
 
         // Libraries
         require_once( SWIPEGO_WC_PATH . 'libraries/swipego/class-swipego.php' );
-
-        // Functions
-        require_once( SWIPEGO_WC_PATH . 'includes/functions.php' );
-
-        // Admin
-        require_once( SWIPEGO_WC_PATH . 'admin/class-swipego-wc-admin.php' );
-
+        
         if ( swipego_is_plugin_activated( 'woocommerce/woocommerce.php' ) ) {
+            
+            // Functions
+            require_once( SWIPEGO_WC_PATH . 'includes/functions.php' );
+    
+            // Admin
+            require_once( SWIPEGO_WC_PATH . 'admin/class-swipego-wc-admin.php' );
 
             // API
             require_once( SWIPEGO_WC_PATH . 'libraries/swipego/includes/abstracts/abstract-swipego-client.php' );
             require_once( SWIPEGO_WC_PATH . 'libraries/swipego/includes/class-swipego-api.php' );
             require_once( SWIPEGO_WC_PATH . 'includes/class-swipego-wc-api.php' );
 
+            if ( swipego_get_integration() ) {
+                
+                // Initialize payment gateway
+                require_once( SWIPEGO_WC_PATH . 'includes/class-swipego-wc-init.php' );
+                
+            }
+            
+            if ( swipego_is_logged_in() ) {
+                
+                // Settings
+                require_once( SWIPEGO_WC_PATH . 'admin/class-swipego-wc-settings.php' );
+                
+            }
         }
 
-        if ( swipego_get_integration() ) {
-            
-            // Initialize payment gateway
-            require_once( SWIPEGO_WC_PATH . 'includes/class-swipego-wc-init.php' );
-            
-        }
         
-        if ( swipego_is_logged_in() ) {
-            
-            // Settings
-            require_once( SWIPEGO_WC_PATH . 'admin/class-swipego-wc-settings.php' );
-            
-        }
 
     }
 
